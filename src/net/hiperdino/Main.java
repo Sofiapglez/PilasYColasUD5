1b0f25064423e9a86d06c3c8b92c0ceb04b6e0e5
Sofiapglez <126815471+Sofiapglez@users.noreply.github.com>
2023-05-04 22:40:25 +0100

    feat(Cajero): métodos cajero

--- src/net/hiperdino/Main.java
+++ src/net/hiperdino/Main.java
@@ -1,7 +1,32 @@
 package net.hiperdino;
 
+import net.hiperdino.staff.Cajero;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cajero cajero = new Cajero();
+        Scanner scanner = new Scanner(System.in);
+        boolean cajaAbierta = false;
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println("Escoge una opción:  ");
+            System.out.println("1. Abrir caja");
+            System.out.println("2. Añadir un nuevo cliente a la cola");
+            System.out.println("3. Atender un cliente");
+            System.out.println("4. Ver clientes pendientes");
+            System.out.println("5. Cerrar supermercado");
+
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    if (cajaAbierta) {
+                        System.out.println("La caja ya está abierta");
+                    } else {
+                    }
+            }
+        }
     }
 }
--- src/net/hiperdino/staff/Cajero.java
+++ src/net/hiperdino/staff/Cajero.java
@@ -1,4 +1,29 @@
 package net.hiperdino.staff;
 
+import net.hiperdino.clientes.Cliente;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cajero {
+    private String nombre;
+    private Queue<Cliente> filaClientes;
+
+    public Cajero(String nombre) {
+        this.nombre = nombre;
+        this.filaClientes = new LinkedList<>();
+    }
+
+    public void agregarCliente(Cliente cliente) {
+        filaClientes.add(cliente);
+    }
+
+    public Cliente atenderCliente() {
+        return filaClientes.poll();
+    }
+
+    public int getSizeFilaClientes() {
+        return filaClientes.size();
+    }
 }
+
