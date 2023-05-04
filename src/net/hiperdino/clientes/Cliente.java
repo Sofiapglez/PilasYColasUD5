9d5a40afd85a367418923926ba5fa84cf6f9ff4c
Sofiapglez <126815471+Sofiapglez@users.noreply.github.com>
2023-05-04 21:49:48 +0100

    feat(Cliente): clase cliente métodos

--- src/net/hiperdino/clientes/Cliente.java
+++ src/net/hiperdino/clientes/Cliente.java
@@ -1,4 +1,30 @@
 package net.hiperdino.clientes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private List<String> cestaCompra;
+
+    public Cliente(String nombre, String apellidos, String dni, List<String> cestaCompra) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.cestaCompra = new ArrayList<>();
+    }
+
+    public void agregarProductoACesta(String producto) {
+        cestaCompra.add(producto);
+    }
+
+    public void eliminarProductoDeCesta(String producto) {
+        cestaCompra.remove(producto);
+    }
+
+    public List <String> getCestaCompra() {
+        return cestaCompra;
+    }
 }
